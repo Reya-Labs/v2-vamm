f6f16a4e45cea80d15ca8b976ab892096444485d
CyclopsRex <cyclopsrex@voltz.xyz>
2023-03-07 16:06:15 +0000

    Upgrade `hardhat` to v2.13
    Enable intermediate representation (IR) iduring compilation, per https://github.com/NomicFoundation/hardhat/releases/tag/hardhat%402.13.0, because IR sidesteps some "stack too deep" errors that would otherwise require maintainability compromises to resolve.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        details: {
+          yulDetails: {
+            optimizerSteps: "u",
+          },
+        },
+      },
+    },
+  },
 };
 
 export default config;
