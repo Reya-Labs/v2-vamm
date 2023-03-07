ffc73cd7dabc47154c56006b184273261036cd3d
CyclopsRex <cyclopsrex@voltz.xyz>
2023-03-07 15:31:17 +0000

    Move Position handling into the VAMM and update exernal interfaces

--- contracts/VAMM/storage/DatedIrsVamm.sol
+++ contracts/VAMM/storage/DatedIrsVamm.sol
@@ -33,6 +33,42 @@ library DatedIrsVamm {
      */
     error IRSVammNotFound(uint128 vammId);
 
+    struct Position { // TODO: LP Position, unlike V1 where it could also be trader
+        uint128 accountId; // TODO: should be of custom type account ID (uint128)
+        /** 
+        * @dev position notional amount
+        */
+        int128 baseAmount;
+        /** 
+        * @dev lower tick boundary of the position
+        */
+        int24 tickLower;
+        /** 
+        * @dev upper tick boundary of the position
+        */
+        int24 tickUpper;
+        /** 
+        * @dev fixed token growth per unit of liquidity as of the last update to liquidity or fixed/variable token balance
+        */
+        int256 tracker0UpdatedGrowth;
+        /** 
+        * @dev variable token growth per unit of liquidity as of the last update to liquidity or fixed/variable token balance
+        */
+        int256 tracker1UpdatedGrowth;
+        /** 
+        * @dev current Fixed Token balance of the position, 1 fixed token can be redeemed for 1% APY * (annualised amm term) at the maturity of the amm
+        * assuming 1 token worth of notional "deposited" in the underlying pool at the inception of the amm
+        * can be negative/positive/zero
+        */
+        int256 tracker0Accumulated;
+        /** 
+        * @dev current Variable Token Balance of the position, 1 variable token can be redeemed for underlyingPoolAPY*(annualised amm term) at the maturity of the amm
+        * assuming 1 token worth of notional "deposited" in the underlying pool at the inception of the amm
+        * can be negative/positive/zero
+        */
+        int256 tracker1Accumulated;
+    }
+
     struct Data {
         /**
          * @dev Numeric identifier for the vamm. Must be unique.
@@ -55,8 +91,16 @@ library DatedIrsVamm {
          * See onlyVAMMOwner.
          */
         address owner;
+        /**
+         * @dev Maps from position ID (see `getPositionId` to the properties of that position
+         */
+        mapping(uint256 => Position) positions;
+        /**
+         * @dev Maps from an account address to a list of the position IDs of positions associated with that account address. Use the `positions` mapping to see full details of any given `Position`.
+         */
+        mapping(uint128 => uint256[]) positionsInAccount;
 
-        address gtwapOracle; // replace with GWAP interface
+        address gtwapOracle; // TODO: replace with GWAP interface
         uint256 termEndTimestampWad;
         uint128 _maxLiquidityPerTick;
         bool _unlocked; // Mutex
@@ -97,6 +141,126 @@ library DatedIrsVamm {
         if (DatedIrsVamm.load(vammId).owner != caller) {
             revert AccessError.Unauthorized(caller);
         }
+    }
+
+    /**
+     * @notice Executes a dated maker order that provides liquidity this VAMM
+     * @param accountId Id of the `Account` with which the lp wants to provide liqudiity
+     * @param fixedRateLower Lower Fixed Rate of the range order
+     * @param fixedRateUpper Upper Fixed Rate of the range order
+     * @param requestedBaseAmount Requested amount of notional provided to a given vamm in terms of the virtual base tokens of the
+     * market
+     * @param executedBaseAmount Executed amount of notional provided to a given vamm in terms of the virtual base tokens of the
+     * market
+     */
+    function executeMakerOrder( // TODO: should change to initiateMakerOrder. C&P interface shape from product
+        Data storage self,
+        uint128 accountId,
+        uint160 fixedRateLower,
+        uint160 fixedRateUpper,
+        int128 requestedBaseAmount
+    )
+        internal
+        returns (int256 executedBaseAmount){
+        
+        int24 tickLower = TickMath.getTickAtSqrtRatio(fixedRateUpper);
+        int24 tickUpper = TickMath.getTickAtSqrtRatio(fixedRateLower);
+
+        uint256 positionId = openPosition(self, accountId, tickLower, tickUpper); // TODO: it probably needn't be msg.sender, but when it's not we need to authenticate
+
+        Position memory position = getRawPosition(self, positionId);
+
+        require(position.baseAmount + requestedBaseAmount >= 0, "Burning too much");
+
+        vammMint(self, msg.sender, tickLower, tickUpper, requestedBaseAmount);
+
+        self.positions[positionId].baseAmount += requestedBaseAmount;
+       
+        return requestedBaseAmount;
+    }
+
+    /**
+     * @notice It opens a position and returns positionId
+     */
+    function openPosition(
+        Data storage self,
+        uint128 accountId,
+        int24 tickLower,
+        int24 tickUpper
+    )
+        internal
+        returns (uint256){
+
+        uint256 positionId = getPositionId(accountId, tickLower, tickUpper);
+
+        if(self.positions[positionId].accountId == 0) {
+            return positionId;
+        }
+
+        self.positions[positionId].accountId = accountId;
+        self.positions[positionId].tickLower = tickLower;
+        self.positions[positionId].tickUpper = tickUpper;
+
+        self.positionsInAccount[accountId].push(positionId);
+
+        return positionId;
+    }
+
+    function getRawPosition(
+        Data storage self,
+        uint256 positionId
+    )
+        internal
+        returns (Position memory) {
+
+        // TODO: check/enforce assertion that zero is not a valid account ID
+        require(self.positions[positionId].accountId != 0, "Missing position");
+        
+        propagatePosition(self, positionId);
+        return self.positions[positionId];
+    }
+
+    function propagatePosition(
+        Data storage self,
+        uint256 positionId
+    )
+        internal {
+
+        Position memory position = self.positions[positionId];
+
+        (int256 tracker0GlobalGrowth, int256 tracker1GlobalGrowth) = 
+            growthBetweenTicks(self, position.tickLower, position.tickUpper);
+
+        int256 tracket0DeltaGrowth =
+                tracker0GlobalGrowth - position.tracker0UpdatedGrowth;
+        int256 tracket1DeltaGrowth =
+                tracker1GlobalGrowth - position.tracker1UpdatedGrowth;
+
+        int256 averageBase = DatedIrsVamm.getAverageBase(
+            position.tickLower,
+            position.tickUpper,
+            position.baseAmount
+        );
+
+        self.positions[positionId].tracker0UpdatedGrowth = tracker0GlobalGrowth;
+        self.positions[positionId].tracker1UpdatedGrowth = tracker1GlobalGrowth;
+        self.positions[positionId].tracker0Accumulated += tracket0DeltaGrowth * averageBase;
+        self.positions[positionId].tracker1Accumulated += tracker1GlobalGrowth * averageBase;
+    }
+
+    /**
+     * @notice Returns the positionId that such a position would have, shoudl it exist. Does not check for existence.
+     */
+    function getPositionId(
+        uint128 accountId,
+        int24 tickLower,
+        int24 tickUpper
+    )
+        public
+        pure
+        returns (uint256){
+
+        return uint256(keccak256(abi.encodePacked(accountId, tickLower, tickUpper)));
     }
 
     function changePauser(Data storage self, address account, bool permission) internal {
@@ -453,6 +617,49 @@ library DatedIrsVamm {
 
         tracker0GrowthOutside = trackFixedTokens(averageBase, tickLower, tickUpper, self.termEndTimestampWad);
         tracker1GrowthOutside = averageBase;
+
+    }
+
+    function getAccountUnfilledBases(
+        Data storage self,
+        uint128 accountId
+    )
+        internal
+        returns (int256 unfilledBaseLong, int256 unfilledBaseShort)
+    {
+        uint256 numPositions = self.positionsInAccount[accountId].length;
+        if (numPositions != 0) {
+            for (uint256 i = 0; i < numPositions; i++) {
+                Position memory position = getRawPosition(self, self.positionsInAccount[accountId][i]); // TODO: Only positions for given subpool / VAMM
+
+                (int256 unfilledLong,, int256 unfilledShort,) = trackValuesBetweenTicks(
+                    self,
+                    position.tickLower,
+                    position.tickUpper,
+                    position.baseAmount
+                );
+
+                unfilledBaseLong += unfilledLong;
+                unfilledBaseShort += unfilledShort;
+            }
+        }
+    }
+
+    function getAccountFilledBalances(
+        Data storage self,
+        uint128 accountId
+    )
+        external
+        returns (int256 baseBalancePool, int256 quoteBalancePool) {
+        
+        uint256 numPositions = self.positionsInAccount[accountId].length;
+
+        for (uint256 i = 0; i < numPositions; i++) {
+            Position memory position = getRawPosition(self, self.positionsInAccount[accountId][i]); 
+
+            baseBalancePool += position.tracker0Accumulated;
+            quoteBalancePool += position.tracker1Accumulated;
+        }
 
     }
 
--- contracts/VAMM/storage/DatedIrsVammPool.sol
+++ contracts/VAMM/storage/DatedIrsVammPool.sol
@@ -8,42 +8,6 @@ import "../libraries/VAMMBase.sol";
 library DatedIrsVammPool {
     using DatedIrsVamm for DatedIrsVamm.Data;
 
-    struct Position {
-        address accountId;
-        /** 
-        * @dev position notional amount
-        */
-        int128 baseAmount;
-        /** 
-        * @dev lower tick boundary of the position
-        */
-        int24 tickLower;
-        /** 
-        * @dev upper tick boundary of the position
-        */
-        int24 tickUpper;
-        /** 
-        * @dev fixed token growth per unit of liquidity as of the last update to liquidity or fixed/variable token balance
-        */
-        int256 tracker0UpdatedGrowth;
-        /** 
-        * @dev variable token growth per unit of liquidity as of the last update to liquidity or fixed/variable token balance
-        */
-        int256 tracker1UpdatedGrowth;
-        /** 
-        * @dev current Fixed Token balance of the position, 1 fixed token can be redeemed for 1% APY * (annualised amm term) at the maturity of the amm
-        * assuming 1 token worth of notional "deposited" in the underlying pool at the inception of the amm
-        * can be negative/positive/zero
-        */
-        int256 tracker0Accumulated;
-        /** 
-        * @dev current Variable Token Balance of the position, 1 variable token can be redeemed for underlyingPoolAPY*(annualised amm term) at the maturity of the amm
-        * assuming 1 token worth of notional "deposited" in the underlying pool at the inception of the amm
-        * can be negative/positive/zero
-        */
-        int256 tracker1Accumulated;
-    }
-
     struct Data {
         /**
          * @dev Numeric identifier for the vamm. Must be unique.
@@ -63,18 +27,7 @@ library DatedIrsVammPool {
          */
         address owner;
 
-        address gtwapOracle;
-
-        /**
-         * @dev Maps from position ID (see `getPositionId` to the properties of that position
-         */
-        mapping(uint256 => Position) positions;
-        /**
-         * @dev Maps from an account address to a list of the position IDs of positions associated with that account address. Use the `positions` mapping to see full details of any given `Position`.
-         */
-        mapping(address => uint256[]) positionsInAccount;
-
-        mapping(uint256 => bool) supportedMaturities;
+        mapping(uint256 => bool) supportedMaturities; // TODO: get rid of this - need to somehow check validity of market, maturity pair (could be done when loading?)
     }
 
     function load(uint128 id) internal pure returns (Data storage self) {
@@ -94,7 +47,7 @@ library DatedIrsVammPool {
         int256 baseAmount,
         uint160 priceLimit
     )
-        internal
+        external
         returns (int256 executedBaseAmount, int256 executedQuoteAmount){
 
         require(self.supportedMaturities[maturityTimestamp], "Maturity not supported");
@@ -103,7 +56,7 @@ library DatedIrsVammPool {
         DatedIrsVamm.Data storage vamm = DatedIrsVamm.loadByMaturityAndMarket(marketId, maturityTimestamp);
 
         VAMMBase.SwapParams memory swapParams;
-        swapParams.recipient = msg.sender;
+        swapParams.recipient = msg.sender; // TODO: not needed. No account ID needed either.
         swapParams.amountSpecified = baseAmount;
         swapParams.sqrtPriceLimitX96 = priceLimit == 0
                 ? (
@@ -120,6 +73,7 @@ library DatedIrsVammPool {
 
     /**
      * @notice Executes a dated maker order against a vamm that provided liquidity to a given marketId & maturityTimestamp pair
+     * @param accountId Id of the `Account` with which the lp wants to provide liqudiity
      * @param marketId Id of the market in which the lp wants to provide liqudiity
      * @param maturityTimestamp Timestamp at which a given market matures
      * @param fixedRateLower Lower Fixed Rate of the range order
@@ -129,175 +83,47 @@ library DatedIrsVammPool {
      * @param executedBaseAmount Executed amount of notional provided to a given vamm in terms of the virtual base tokens of the
      * market
      */
-    function executeDatedMakerOrder(
-        Data storage self,
+    function initiateMakerOrder( // TODO: should change to initiateMakerOrder. C&P interface shape from product
+        uint128 accountId,
         uint128 marketId,
         uint256 maturityTimestamp,
         uint160 fixedRateLower,
         uint160 fixedRateUpper,
         int128 requestedBaseAmount
     )
-        internal
-        returns (int256 executedBaseAmount){
+        external
+        returns (int256 executedBaseAmount){ // TODO: returning 256 for 128 request seems wrong
         
-       require(self.supportedMaturities[maturityTimestamp], "Maturity not supported");
+        // TODO: check if market+maturity combo is supported
 
-        int24 tickLower = TickMath.getTickAtSqrtRatio(fixedRateUpper);
-        int24 tickUpper = TickMath.getTickAtSqrtRatio(fixedRateLower);
-
-       uint256 positionId = openPosition(self, msg.sender, tickLower, tickUpper); // TODO: it probably needn't be msg.sender, but when it's not we need to authenticate
-
-        // what happens if vamm for maturity & market does not exist?
+        // TODO: what happens if vamm for maturity & market does not exist?
        DatedIrsVamm.Data storage vamm = DatedIrsVamm.loadByMaturityAndMarket(marketId, maturityTimestamp);
-       Position memory position = getRawPosition(self, positionId, vamm);
-
-       require(position.baseAmount + requestedBaseAmount >= 0, "Burning too much");
-
-       vamm.vammMint(msg.sender, tickLower, tickUpper, requestedBaseAmount);
-
-       self.positions[positionId].baseAmount += requestedBaseAmount;
-       
-        return requestedBaseAmount;
+       return vamm.executeMakerOrder(accountId, fixedRateLower, fixedRateUpper, requestedBaseAmount);
     }
 
     function getAccountFilledBalances(
-        Data storage self,
         uint128 marketId,
         uint256 maturityTimestamp,
-        address accountId
+        uint128 accountId
     )
-        internal
+        external
         returns (int256 baseBalancePool, int256 quoteBalancePool) {
         
-        require(self.supportedMaturities[maturityTimestamp], "Maturity not supported");
-
-        uint256 numPositions = self.positionsInAccount[accountId].length;
-
+        // TODO: what if market+maturity not supported?
         DatedIrsVamm.Data storage vamm = DatedIrsVamm.loadByMaturityAndMarket(marketId, maturityTimestamp);
-        for (uint256 i = 0; i < numPositions; i++) {
-            Position memory position = getRawPosition(self, self.positionsInAccount[accountId][i], vamm);
-
-            baseBalancePool += position.tracker0Accumulated;
-            quoteBalancePool += position.tracker1Accumulated;
-        }
-
+        return vamm.getAccountFilledBalances(accountId);
     }
 
     function getAccountUnfilledBases(
-        Data storage self,
         uint128 marketId,
         uint256 maturityTimestamp,
-        address accountId
+        uint128 accountId
     )
-        internal
+        external
         returns (int256 unfilledBaseLong, int256 unfilledBaseShort)
     {
-        
-        require(self.supportedMaturities[maturityTimestamp], "Maturity not supported");
-
-        uint256 numPositions = self.positionsInAccount[accountId].length;
-        if (numPositions != 0) {
-            DatedIrsVamm.Data storage vamm = DatedIrsVamm.loadByMaturityAndMarket(marketId, maturityTimestamp);
-
-            for (uint256 i = 0; i < numPositions; i++) {
-                Position memory position = getRawPosition(self, self.positionsInAccount[accountId][i], vamm);
-
-                (int256 unfilledLong,, int256 unfilledShort,) = vamm.trackValuesBetweenTicks(
-                    position.tickLower,
-                    position.tickUpper,
-                    position.baseAmount
-                );
-
-                unfilledBaseLong += unfilledLong;
-                unfilledBaseShort += unfilledShort;
-            }
-        }
+        // TODO: what if market+maturity combo does not exist?
+        DatedIrsVamm.Data storage vamm = DatedIrsVamm.loadByMaturityAndMarket(marketId, maturityTimestamp);
+        return vamm.getAccountUnfilledBases(accountId);
     }
-
-    /**
-     * @notice It opens a position and returns positionId
-     */
-    function openPosition(
-        Data storage self,
-        address accountId,
-        int24 tickLower,
-        int24 tickUpper
-    )
-        internal
-        returns (uint256){
-
-        uint256 positionId = getPositionId(accountId, tickLower, tickUpper);
-
-        if(self.positions[positionId].accountId == address(0)) {
-            return positionId;
-        }
-
-        self.positions[positionId].accountId = accountId;
-        self.positions[positionId].tickLower = tickLower;
-        self.positions[positionId].tickUpper = tickUpper;
-
-        self.positionsInAccount[accountId].push(positionId);
-
-        return positionId;
-    }
-
-    /**
-     * @notice Returns the positionId that such a position would have, shoudl it exist. Does not check for existence.
-     */
-    function getPositionId(
-        address accountId,
-        int24 tickLower,
-        int24 tickUpper
-    )
-        public
-        pure
-        returns (uint256){
-
-        return uint256(keccak256(abi.encodePacked(accountId, tickLower, tickUpper)));
-    }
-
-    function getRawPosition(
-        Data storage self,
-        uint256 positionId,
-        DatedIrsVamm.Data storage vamm
-    )
-        internal
-        returns (Position memory) {
-
-        require(self.positions[positionId].accountId != address(0), "Missing position");
-        
-        propagatePosition(self, positionId, vamm);
-        return self.positions[positionId];
-    }
-
-    function propagatePosition(
-        Data storage self,
-        uint256 positionId,
-        DatedIrsVamm.Data storage vamm
-    )
-        internal {
-
-        Position memory position = self.positions[positionId];
-
-        (int256 tracker0GlobalGrowth, int256 tracker1GlobalGrowth) = 
-            vamm.growthBetweenTicks(position.tickLower, position.tickUpper);
-
-        int256 tracket0DeltaGrowth =
-                tracker0GlobalGrowth - position.tracker0UpdatedGrowth;
-        int256 tracket1DeltaGrowth =
-                tracker1GlobalGrowth - position.tracker1UpdatedGrowth;
-
-        int256 averageBase = DatedIrsVamm.getAverageBase(
-            position.tickLower,
-            position.tickUpper,
-            position.baseAmount
-        );
-
-        self.positions[positionId].tracker0UpdatedGrowth = tracker0GlobalGrowth;
-        self.positions[positionId].tracker1UpdatedGrowth = tracker1GlobalGrowth;
-        self.positions[positionId].tracker0Accumulated += tracket0DeltaGrowth * averageBase;
-        self.positions[positionId].tracker1Accumulated += tracker1GlobalGrowth * averageBase;
-    }
-
-            
 }
