490b9897e95c5844bd973dbed36cc9181d2f333f
CyclopsRex <cyclopsrex@voltz.xyz>
2023-03-21 14:58:19 +0000

    Test openPosition

--- test/DatedIrsVammTest.sol
+++ test/DatedIrsVammTest.sol
@@ -2,53 +2,62 @@ pragma solidity >=0.8.13;
 
 import "forge-std/Test.sol";
 import "../contracts/VAMM/storage/DatedIrsVAMM.sol";
+import { UD60x18, convert } from "@prb/math/src/UD60x18.sol";
+import { SD59x18, convert } from "@prb/math/src/SD59x18.sol";
 
 
 // contract ExposedDatedIrsVamm is CoreState {
-contract ExposedDatedIrsVamm {
-    using DatedIrsVamm for DatedIrsVamm.Data;
-    DatedIrsVamm.Data public vamm;
+// contract ExposedDatedIrsVamm {
+//     using DatedIrsVamm for DatedIrsVamm.Data;
+//     DatedIrsVamm.Data public vamm;
 
 
-    function initialize(uint160 sqrtPriceX96, uint256 _termEndTimestamp, uint128 _marketId, int24 _tickSpacing, DatedIrsVamm.DatedIrsVAMMConfig memory _config) internal {
-        vamm.initialize(sqrtPriceX96, _termEndTimestamp, _marketId, _tickSpacing,  _config);
-        // emit Initialize(sqrtPriceX96, tick); // TODO: emit log for new VAMM, either here or in DatedIrsVAMMPool
-    }
+//     function initialize(uint160 sqrtPriceX96, uint256 _termEndTimestamp, uint128 _marketId, int24 _tickSpacing, DatedIrsVamm.DatedIrsVAMMConfig memory _config) internal {
+//         vamm.initialize(sqrtPriceX96, _termEndTimestamp, _marketId, _tickSpacing,  _config);
+//         // emit Initialize(sqrtPriceX96, tick); // TODO: emit log for new VAMM, either here or in DatedIrsVAMMPool
+//     }
 
-    // Exposed functions
-    // function load(uint128 id) external {
-    //     vamm = DatedIrsVamm.load(id);
-    // }
+//     // Exposed functions
+//     // function load(uint128 id) external {
+//     //     vamm = DatedIrsVamm.load(id);
+//     // }
 
-    // function create(uint128 _marketId, uint256 _maturityTimestamp,  uint160 _sqrtPriceX96, int24 _tickSpacing, DatedIrsVamm.DatedIrsVAMMConfig memory _config) external returns (bytes32 s) {
-    //     DatedIrsVamm.Data storage account = Account.create(id, owner);
-    //     assembly {
-    //         s := account.slot
-    //     }
-    // }
+//     // function create(uint128 _marketId, uint256 _maturityTimestamp,  uint160 _sqrtPriceX96, int24 _tickSpacing, DatedIrsVamm.DatedIrsVAMMConfig memory _config) external returns (bytes32 s) {
+//     //     DatedIrsVamm.Data storage account = Account.create(id, owner);
+//     //     assembly {
+//     //         s := account.slot
+//     //     }
+//     // }
 
-    function propagatePosition(uint256 positionId) external {
-        vamm.propagatePosition(positionId);
-    }
+//     function propagatePosition(uint256 positionId) external {
+//         vamm.propagatePosition(positionId);
+//     }
 
-    function executeDatedMakerOrder(uint128 accountId,
-        uint160 fixedRateLower,
-        uint160 fixedRateUpper,
-        int128 requestedBaseAmount) external returns (int256 executedBaseAmount) {
-        return vamm.executeDatedMakerOrder(accountId,
-        fixedRateLower,
-        fixedRateUpper,
-        requestedBaseAmount);
-    }
-}
+//     function executeDatedMakerOrder(uint128 accountId,
+//         uint160 fixedRateLower,
+//         uint160 fixedRateUpper,
+//         int128 requestedBaseAmount) external returns (int256 executedBaseAmount) {
+//         return vamm.executeDatedMakerOrder(accountId,
+//         fixedRateLower,
+//         fixedRateUpper,
+//         requestedBaseAmount);
+//     }
+// }
 
 contract VammTest is Test {
+    using DatedIrsVamm for DatedIrsVamm.Data;
+    uint256 latestPositionId;
     uint256 testNumber;
-    ExposedDatedIrsVamm internal vamm;
-    uint256 constant testPositionId = 1;
+    DatedIrsVamm.Data internal vamm;
+    DatedIrsVamm.DatedIrsVAMMConfig internal config = DatedIrsVamm.DatedIrsVAMMConfig({
+        priceImpactPhi: convert(uint256(0)),
+        priceImpactBeta: convert(uint256(0)),
+        spread: convert(uint256(0)),
+        rateOracle: IRateOracle(address(0))
+    });
 
     function setUp() public {
-        vamm = new ExposedDatedIrsVamm();
+        vamm.initialize(uint160(FixedPoint96.Q96), block.timestamp+100, 0, 100, config);
         testNumber = 42;
     }
 
@@ -61,12 +70,18 @@ contract VammTest is Test {
     }
 
     function testFail_GetUnopenedPosition() public {
-        vamm.getRawPosition(testPositionId);
+        vamm.getRawPosition(1);
     }
 
     function test_OpenPosition() public {
-        uint256 openedPosition = vamm.openPosition(1,1,3);
-
-        assertEq(openedPosition, 1);
+        uint128 accountId = 1;
+        int24 tickLower = 2;
+        int24 tickUpper = 3;
+        latestPositionId = vamm.openPosition(accountId,tickLower,tickUpper);
+        assertEq(latestPositionId, DatedIrsVamm.getPositionId(accountId,tickLower,tickUpper));
+        assertEq(vamm.positions[latestPositionId].accountId, accountId);
+        assertEq(vamm.positions[latestPositionId].tickLower, tickLower);
+        assertEq(vamm.positions[latestPositionId].tickUpper, tickUpper);
+        vamm.getRawPosition(latestPositionId);
     }
 }
