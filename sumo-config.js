bd822a613a9855ad129c0f4809447c7a7d229a60
CyclopsRex <cyclopsrex@voltz.xyz>
2023-04-20 08:32:49 +0100

    Install SuMo contract mutator (https://github.com/MorenaBarboni/SuMo-SOlidity-MUtator)

---
+++ sumo-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  buildDir: "out",
+  contractsDir: "contracts",
+  testDir: "test",
+  skipContracts: [],
+  skipTests: [],
+  testingTimeOutInSec: 300,
+  network: "none",
+  testingFramework: "forge",
+  optimized: true,
+  tce: true,
+};
