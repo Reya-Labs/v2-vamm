48d08f0e746f6e04f7bb9288d1a198d6413c3892
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-04-30 18:52:16 +0100

    feat: add vamm proxy interface

---
+++ src/IVAMMProxy.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "./interfaces/IAccountBalanceModule.sol";
+import "./interfaces/IPoolModule.sol";
+import "./interfaces/IPoolPauserModule.sol";
+import "./interfaces/IVammModule.sol";
+import "@voltz-protocol/util-contracts/src/interfaces/IOwnable.sol";
+import "@voltz-protocol/util-modules/src/interfaces/IBaseOwnerModule.sol";
+import "@voltz-protocol/util-contracts/src/interfaces/IUUPSImplementation.sol";
+
+interface IVAMMProxy is 
+  IOwnable,
+  IBaseOwnerModule,
+  IUUPSImplementation,
+  IAccountBalanceModule,
+  IPoolModule,
+  IPoolPauserModule,
+  IVammModule { }
